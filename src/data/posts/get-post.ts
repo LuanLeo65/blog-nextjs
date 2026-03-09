22b59675ee89512a2e248a0430f2f51aef4449a9
LuanLeo <luan@leo.com>
2026-03-09 08:28:44 -0300

    convertendo o content markdown em html

--- src/data/posts/get-post.ts
+++ src/data/posts/get-post.ts
@@ -1,11 +1,14 @@
 import { POSTS_URL } from "@/config/app-config";
 import { PostData } from "@/domain/posts/post";
 import { fetchJson } from "@/utils/fetch-json";
+import { markdownToHtml } from "@/utils/markdown-to-html";
 
 export const getPost = async (slug: string | string[]): Promise<PostData[]> => {
   const slugString = Array.isArray(slug) ? slug[0] : slug
     const url = `${POSTS_URL}&filters[slug][$eq]=${slugString}`
     const jsonPost = await fetchJson<PostData[]>(url)
-    return jsonPost
+    const content = await markdownToHtml(jsonPost[0].content)
+    const finalContent = {...jsonPost[0], content}
+    return [finalContent]
 
 }
