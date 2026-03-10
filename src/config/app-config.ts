74dca6752fac38542c2f6ccfde44cbc5a9f320f3
LuanLeo <luan@leo.com>
2026-03-10 15:07:46 -0300

    corrigindo url de producao

--- src/config/app-config.ts
+++ src/config/app-config.ts
@@ -2,4 +2,4 @@ export const API_URL = "https://strapits.onrender.com/api"
 export const POSTS_URL = `${API_URL}/posts?populate=*`
 
 export const SITE_NAME = 'Blog'
-export const SITE_URL = 'https://69b05c6220e3768d3ded836d--elegant-lamington-5d6aa4.netlify.app'
+export const SITE_URL = 'https://elegant-lamington-5d6aa4.netlify.app'
